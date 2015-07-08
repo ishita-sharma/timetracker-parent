e2a9ad222dfbbafe36909943b6a81deaaf7345f8
ishita-sharma <ishi18sharma@gmail.com>
2015-07-08 22:15:49 +0200

    implemented Jackson, decoupled model

--- timetracker-web/src/main/java/de/ishitasharma/timetracker/controller/HealthController.java
+++ timetracker-web/src/main/java/de/ishitasharma/timetracker/controller/HealthController.java
@@ -1,6 +1,7 @@
 package de.ishitasharma.timetracker.controller;
 
 import java.io.IOException;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +9,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import de.ishitasharma.timetracker.model.Health;
+
 @Controller
 @RequestMapping("/health")
 public class HealthController {
 
-	@RequestMapping(value="/check",
-			method={RequestMethod.GET},
-	produces={MediaType.APPLICATION_JSON_VALUE})
+	@RequestMapping(value = "/check", method = { RequestMethod.GET }, produces = { MediaType.APPLICATION_JSON_VALUE })
 	@ResponseBody
-	public String healthCheck(@RequestParam(value="name",defaultValue="Ishita")String name) throws IOException{
-//		PrintWriter ps = response.getWriter();
-//		ps.write("ok");
-//		ps.close();
-		return "{\"status\":\"ok\",\"message\":\"Welcome "+name+"\"}";
+	public String healthCheck(
+			@RequestParam(value = "message", defaultValue = "Ishita") String message,
+			@RequestParam(value = "status", defaultValue = "error") String status)
+			throws IOException {
+		return new Health(message, status).toString();
 	}
 }
---
+++ timetracker-web/src/main/java/de/ishitasharma/timetracker/model/Health.java
@@ -0,0 +1,53 @@
+package de.ishitasharma.timetracker.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class Health {
+
+//	jackson pojo json response rest
+	
+	@JsonProperty("STATUS")
+	private String status;
+	@JsonProperty("MESSAGE")
+	private String message;
+	
+	public Health(){
+		
+	}
+	
+	public Health(String message, String status){
+		this.message=message;
+		this.status=status;
+	}
+	
+	@Override
+	public String toString(){
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			return "Cannot parse object into Json";
+		}
+	}
+	
+	//getter and setter
+	
+	public void setStatus(String status){
+		this.status = status;
+	}
+	
+	public String getStatus(){
+		return status;
+	}
+	
+	public void setMessage(String msg){
+		message = msg;
+	}
+	
+	public String getMessage(){
+		return message;
+	}
+}
